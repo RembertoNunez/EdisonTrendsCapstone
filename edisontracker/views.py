498fceb51a363d1fd2e1f41a05ed12c87d73f579
Luis Valencia <lvalencia@csumb.edu>
2019-03-20 18:18:41 -0700

    Changed from button functionality

--- edisontracker/views.py
+++ edisontracker/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from django.http import JsonResponse
 
 
 def home(request):
-    return render(request, 'edisontracker/index.html')
+    companyList = ["Burger King", "Jack in the Box", "McDonald", "Taco Bell", "Chipotle Burrito Bar", " Chick-Fil-A", "Wendy's"]
+    companyList.sort()
+    return render(request, 'edisontracker/index.html', {"company_list": companyList})
